d7919af2aad3168ebff6c5c29be6d99a5f5ab161
yonnie-park <juon0805@gmail.com>
2022-12-04 21:51:12 +0900

    commit

--- 28_robotPath2.js
+++ 28_robotPath2.js
@@ -1,40 +1,58 @@
-const robotPath2 = function (room, src, sDir, dst, dDir) {
-    // TODO: 여기에 코드를 작성합니다.
-    const dx = [-1, 0, 1, 0];
-    const dy = [0, 1, 0, -1];
-  
-    const visited = Array.from(Array(room.length), () =>
-      new Array(room[0].length).fill(0)
-    );
-  
-    visited[src[0]][src[1]] = 1;
-    // [[x,y],dir,step]
-    const queue = [[[src[0], src[1]], sDir, 0]]
-    while(queue.length){
-      let current=queue.shift()
-      for(let i=1; i<=4; i++){
-        let nx=x+dx[i]
-        let ny=y+dy[i]
+function robotPath2(room, src, sDir, dst, dDir){
+  let q = [];
+  let dist= [];
 
-        if(nx>=0 && nx<room.length && ny>=0 && ny<room[0].length){
-          //여기에 뭘 해야댐   
+  for(let i=0;i<room.length;i++){
+    dist.push(Array(room[0].length).fill(Infinity));
+  }
+
+  let dx = [,-1,0,1,0];
+  let dy = [,0,1,0,-1];
+
+  q.push([...src,sDir,0]);
+  dist[src[0]][src[1]] = 0;
+
+  const canGo = (x,y) => (x>=0&&x<room.length&&y>=0&&y<room[0].length&&room[x][y]===0);
+  
+  const isDst = (x,y) => (x===dst[0]&&y===dst[1]);
+
+  const getNumOfRotate = (diff) => {
+    if(diff===3) return 1;
+    return diff;
+  }
+  
+  const getNextCount = (diff,count,nextx,nexty,nextDirection) =>{
+        let numOfRotate = getNumOfRotate(diff);
+        
+        if(isDst(nextx,nexty)){
+          let lastRotation = getNumOfRotate(Math.abs(nextDirection-dDir));
+
+          if(numOfRotate===0) return(count===0? count + 1 + lastRotation : count + lastRotation);//방향이 같을 경우 시작점에서는 +1, 아닌경우 +0
+          return numOfRotate + count + 1 + lastRotation;
+        }
+        else{
+          if(numOfRotate===0) return(count===0? count + 1 : count);//방향이 같을 경우 시작점에서는 +1, 아닌경우 +0
+          return numOfRotate + count + 1;
+        }
+  }
+
+  while(q.length>0){
+    let [x,y,direction,count] = q.shift();
+
+    for(let i=1;i<=4;i++){
+      let nextx = x+dx[i];
+      let nexty = y+dy[i];
+
+      if(canGo(nextx,nexty)){
+        let nextCount = getNextCount(Math.abs(direction-i),count,nextx,nexty,i);
+
+        if(nextCount<=dist[nextx][nexty]){
+          dist[nextx][nexty] = nextCount;
+          q.push([nextx,nexty,i,nextCount]);
         }
       }
     }
+  }
+  
+  return dist[dst[0]][dst[1]]
 }
-  const room = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 1, 1, 0, 1],
-    [0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 1, 0, 1, 1, 1],
-    [0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-  ];
-  const src = [0, 4];
-  const sDir = 1;
-  const dst = [7, 0];
-  const dDir = 3;
-  let output = robotPath2(room, src, sDir, dst, dDir);
-  console.log(output);
