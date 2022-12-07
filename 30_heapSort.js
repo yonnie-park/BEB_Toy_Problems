ecfcf72419bfc0f19bb5fe77ec42fe5d93174c91
yonnie-park <juon0805@gmail.com>
2022-12-07 10:39:26 +0900

    add: 31_rangeMinimum.js

--- 30_heapSort.js
+++ 30_heapSort.js
@@ -1,28 +1,28 @@
 // 아래 코드는 수정하지 마세요.
 function swap(idx1, idx2, arr) {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-  }
-  
-  function getParentIdx(idx) {
-    return Math.floor((idx-1)/2)
-  }
-  
-  function insert(heap, item) {
+}
+
+function getParentIdx(idx) {
+    return Math.floor((idx - 1) / 2)
+}
+
+function insert(heap, item) {
     heap.push(item)
     if (heap.length > 1) {
         let curIdx = heap.length - 1;
         let pIdx = getParentIdx(curIdx);
         while (pIdx >= 0 && heap[curIdx] < heap[pIdx]) {
-          swap(curIdx, pIdx, heap);
-          curIdx = pIdx;
-          pIdx = getParentIdx(curIdx);
+            swap(curIdx, pIdx, heap);
+            curIdx = pIdx;
+            pIdx = getParentIdx(curIdx);
         }
-      }
-      return heap;
+    }
+    return heap;
 
-  }
-  
-  function removeRoot(heap) {
+}
+
+function removeRoot(heap) {
     swap(0, heap.length - 1, heap);
     heap.pop();
     if (heap.length === 0) return [];
@@ -35,27 +35,27 @@ function swap(idx1, idx2, arr) {
         let left = curIdx * 2 + 1;
         let right = curIdx * 2 + 2;
         if (left < heap.length && heap[left] < heap[minIdx]) {
-        minIdx = left;
+            minIdx = left;
         }
 
         if (right < heap.length && heap[right] < heap[minIdx]) {
-        minIdx = right;
+            minIdx = right;
         }
 
         swap(curIdx, minIdx, heap);
     }
 
     return heap;
-  }
-  
+}
 
-  const binaryHeap = function (arr) {
+
+const binaryHeap = function(arr) {
     return arr.reduce((heap, item) => {
-      return insert(heap, item);
+        return insert(heap, item);
     }, []);
-  };
-  
-  const heapSort = function (arr) {
+};
+
+const heapSort = function(arr) {
     let minHeap = binaryHeap(arr);
     const sorted = [];
     while (minHeap.length > 0) {
@@ -63,5 +63,4 @@ function swap(idx1, idx2, arr) {
         minHeap = removeRoot(minHeap);
     }
     return sorted;
-  };
-  
+};
---
+++ 31_rangeMinimum.js
@@ -0,0 +1,45 @@
+const rangeMinimum = function(arr, ranges) {
+    // TODO: 여기에 코드를 작성합니다.
+    function initSegmentTree(arr, start, end) {
+        if (start === end) return { value: arr[start] }
+
+        let mid = parseInt((start + end) / 2)
+        let left = initSegmentTree(arr, start, mid)
+        let right = initSegmentTree(arr, mid + 1, end)
+
+        return {
+            value: Math.min(left.value, right.value),
+            left,
+            right,
+        }
+    }
+
+    const tree = initSegmentTree(arr, 0, arr.length - 1)
+
+    function findMin(start, end, rangeStart, rangeEnd, tree) {
+        if (rangeStart <= start && end <= rangeEnd) {
+            return tree.value
+        }
+        if (end < rangeStart || rangeEnd < start) {
+            return Number.MAX_SAFE_INTEGER
+        }
+
+        let mid = parseInt((start + end) / 2)
+        return Math.min(findMin(start, mid, rangeStart, rangeEnd, tree.left), findMin(mid + 1, end, rangeStart, rangeEnd, tree.right))
+    }
+
+    const mins = ranges.map((range) => {
+        let [start, end] = range
+        return findMin(0, arr.length - 1, start, end, tree)
+    })
+
+    return mins
+};
+
+
+const arr = [1, 3, 2, 7, 9, 11];
+const mins = rangeMinimum(arr, [
+    [1, 4],
+    [0, 3],
+]);
+console.log(mins);
