2224b127cd7503b964e604f84db42d8aced7ba5c
yonnie-park <juon0805@gmail.com>
2022-12-10 05:35:59 +0900

    commit

--- 31_rangeMinimum.js
+++ 31_rangeMinimum.js
@@ -1,5 +1,4 @@
 const rangeMinimum = function(arr, ranges) {
-    // TODO: 여기에 코드를 작성합니다.
     function initSegmentTree(arr, start, end) {
         if (start === end) return { value: arr[start] }
 
--- 33_LIS.js
+++ 33_LIS.js
@@ -1,22 +1,14 @@
 const LIS = function(arr) {
-    //TODO: 여기에 코드를 작성합니다.
-    let list = Array(arr.length).fill(0)
-    for (let i = 0; i < arr.length; i++) {
-        list[i] = 1
-    }
-
+    let lis = Array(arr.length).fill(1)
     for (let i = 1; i < arr.length; i++) {
         for (let j = 0; j < i; j++) {
-            if (arr[i] > arr[j] && list[i] < list[j] + 1) {
-                list[i] = list[j] + 1
+            if (arr[i] > arr[j] && lis[i] <= lis[j]) {
+                lis[i] = lis[j] + 1
             }
         }
     }
-    let max = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (max < list[i]) max = list[i]
-    }
+    let max = Math.max(...lis)
     return max
 };
-let output = LIS([3, 2]);
-console.log(output); // --> 1 (3 or 2)
+let output = LIS([3, 10, 2, 1, 20]);
+console.log(output); // --> 3 (3, 10, 20)
